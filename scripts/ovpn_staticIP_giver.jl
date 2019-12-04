ef710b4ac3e29a4e78cc118e07adc599ecf564b4
Akito <akito.kitsune@protonmail.com>
2019-12-04 20:18:11 +0100

    WIP emergency option

--- scripts/ovpn_staticIP_giver.jl
+++ scripts/ovpn_staticIP_giver.jl
@@ -5,6 +5,11 @@
 using Dates
 
 if length(ARGS) >= 1 && ARGS[1] == "init"
+  ## Creates configuration file.
+  ## Backs up old cfg and/or creates new and empty one.
+  ##
+  ###  Usage
+  ## ./ovpn_staticIP_giver.jl init
   println("Create new configuration file?")
   println("If an old configuration file exists it will be backed up.")
   print("Enter YES if you agree: ")
@@ -25,6 +30,44 @@ if length(ARGS) >= 1 && ARGS[1] == "init"
     end
   else
     println("Request denied. Exiting.")
+  end
+elseif length(ARGS) >= 1 && ARGS[1] == "emergency"
+  ## Pray to Satan for this to never be necessary.
+  ##
+  ## Creates static IPs for each client manually.
+  ## Needed if the config folder is lost and
+  ## every config needs to be redone for
+  ## assuring the clients their respective IP.
+  ##
+  ###  Usage
+  ## First, create file "clients.cfg" and
+  ## add each client line by line, like this:
+  ## common-name 10.10.0.13
+  ##
+  ## Just 1 spelling mistake can make at least
+  ## 1 client entry invalid, i.e. this
+  ## emergency recovery wouldn't work.
+  ##
+  ## Once done, run:
+  ## ./ovpn_staticIP_giver.jl emergency
+  function iterClients()
+    clientConfigDir = clientConfigDir
+    clients = open("$clientConfigDirclients.cfg")
+    for line in eachline(clients)
+      #TODO
+      # read common-name
+      # split by space
+      # read IP
+      # write single config file
+    end
+  end
+  try
+    cfg = open("ovpn_staticIP_giver.cfg", "r")
+    clientConfigDir = readline(cfg)
+    close(cfg)
+    
+  catch
+    clientConfigDir = "/var/etc/openvpn/ccd"
   end
 elseif length(ARGS) == 0
   println("placeholder")
