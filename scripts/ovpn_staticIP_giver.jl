ad04306331870748c1e1299998bdcbad1f36ec0f
Akito <akito.kitsune@protonmail.com>
2019-12-06 05:29:46 +0100

    Huge Improvements * explanations * safety nets * everything better

--- scripts/ovpn_staticIP_giver.jl
+++ scripts/ovpn_staticIP_giver.jl
@@ -4,7 +4,13 @@
 
 using Dates
 
-let clientConfigDir::String end
+let clientConfigDir::String,                ## client-config-dir set in OpenVPN Server config.
+    originalIpAddressArray::Array{Int64, 4} ## Starting point in IP address iteration.
+end
+
+#clientConfigDir = "/var/etc/openvpn/ccd"
+# Debugging
+clientConfigDir = "/home/akito/src/julia-serving-hookers/tmp"
 
 if length(ARGS) >= 1 && ARGS[1] == "init"
   ## Creates configuration file.
@@ -53,7 +59,7 @@ elseif length(ARGS) >= 1 && ARGS[1] == "emergency"
   ##
   ## Once done, run:
   ## ./ovpn_staticIP_giver.jl emergency
-  global clientConfigDir = "/var/etc/openvpn/ccd"
+  global clientConfigDir
   function emergeClients()
     println("Run emergency mode?!")
     println("If client-config files exist, they will be overwritten!")
@@ -92,22 +98,59 @@ elseif length(ARGS) >= 1 && ARGS[1] == "emergency"
     emergeClients()
   end
 elseif length(ARGS) == 2 && ARGS[1] == "add"
-  global clientConfigDir = "/var/etc/openvpn/ccd"
-  clients = open("$clientConfigDir/clients.cfg", "r+")
+  ## Adds client by common-name and automatically
+  ## assigns an available static IP address
+  ## permanently to that client.
+  ##
+  ###  Usage
+  ## ./ovpn_staticIP_giver.jl add common-name
+  global clientConfigDir
+  global originalIpAddressArray = [10, 148, 0, 14]
   commonName = ARGS[2]
-  originalIpAddressArray = [10, 148, 0, 14]
+  clients = open("$clientConfigDir/clients.cfg", "r+")
+  clientConfig = open("$clientConfigDir/$commonName", "w+")
   ipAddressArray = originalIpAddressArray
   newIpAddressArray = originalIpAddressArray
-  clientConfig = open("$clientConfigDir/$commonName", "w+")
-  function checkDup()
+  function assignIP()
     position = 4
     ipArc = 0
-    function counter(position, tempIpAddressArray)
+    function findNextFreeIP(position, tempIpAddressArray)
       ipArc = tempIpAddressArray[position]
-      if ipArc == ipAddressArray[position] && ipArc <= 255
+      if position <= -1
+        println("Assigning invalid address.")
+        exit(1)
+      elseif position == 1 && ipArc < 10
+        println("Ran out of IP addresses.")
+        exit(1)
+      end
+      if ipArc == ipAddressArray[position] && ipArc < 255
         tempIpAddressArray[position] = ipArc += 1
+      elseif ipArc >= 255
+        tempIpAddressArray[position] = 1
+        position -= 1
+        findNextFreeIP(position, tempIpAddressArray)
       end
       return tempIpAddressArray
+    end
+    function doubleCheckAssignment()
+      ## Just before assigning the new static
+      ## IP address, it double checks against
+      ## every entry, if it is already registered.
+      for line in eachline(clients)
+        lineArray = split(line, " ")
+        cn = lineArray[1]
+        ipAddress = lineArray[2]
+        tempIpAddressArray = parse.(Int64, split(ipAddress, "."))
+        if newIpAddressArray == tempIpAddressArray
+          println("Assignment invalid.")
+          println("Address already assigned. Exiting.")
+          exit(1)
+        elseif cn == commonName
+          println("Assignment invalid.")
+          println("This Common Name is already registered. Exiting.")
+          exit(1)
+        end
+      end
     end
     for line in eachline(clients)
       ## Reads a config called "clients.cfg" which location
@@ -119,19 +162,41 @@ elseif length(ARGS) == 2 && ARGS[1] == "add"
       ## to re-enable their static IPs.
       global newIpAddressArray
       lineArray = split(line, " ")
-      cn = lineArray[1]
-      ipAddress = lineArray[2]
-      ipAddressArray = parse.(Int64, split(ipAddress, "."))
+      cn = lineArray[1] ## Just to prevent commonName i.e. client duplication.
+      ipAddress = lineArray[2] ## This line's IP address read from cofig file.
+      ipAddressArray = parse.(Int64, split(ipAddress, ".")) ## Converts read IP address to Array of Ints.
       if ipAddressArray == ""
-        println("empty line")
+        ## Ignore empty lines.
+        continue
       elseif commonName == cn
-        println("Client already exists. Exiting.")
-        exit(1)
+        ## Double check to prevent entry duplication.
+        println("Client registered already.")
+        try
+          ## If a file exists and commonName is set in clients.cfg,
+          ## then client is definitely assigned a static IP.
+          f = open("$clientConfigDir/$commonName", "r")
+          close(f)
+          println("Client config file present.")
+        catch
+          ## Both the common-name has to be in the clients.cfg
+          ## and the $commonName client config file must be present.
+          ## If one is missing, something is not right.
+          println("Client registered already, but config file is missing.")
+          println("This needs manual fixing.")
+          exit(1)
+        finally
+          println("Client already exists. Exiting.")
+          exit(1)
+        end
       elseif newIpAddressArray == ipAddressArray
-        global newIpAddressArray = counter(position, newIpAddressArray)
+        ## findNextFreeIP continues iterating until an IP
+        ## is found that has not been registered before.
+        global newIpAddressArray = findNextFreeIP(position, newIpAddressArray)
         continue
       end
     end
+    ## Actual assignment of new static IP addresses
+    doubleCheckAssignment()
     firstByte = newIpAddressArray[1]
     secondByte = newIpAddressArray[2]
     thirdByte = newIpAddressArray[3]
@@ -140,11 +205,11 @@ elseif length(ARGS) == 2 && ARGS[1] == "add"
     write(clientConfig, "ifconfig-push $ipAddressArray $ipAddressArray\n")
     println("Assigned $ipAddressArray to $commonName !")
     write(clients, "$commonName $ipAddressArray\n")
-    println("Added $commonName to $clientConfigDir/$commonName !")
+    println("Added $commonName to $clientConfigDir/clients.cfg !")
     close(clientConfig)
     close(clients)
   end
-  checkDup()
+  assignIP()
 elseif length(ARGS) >= 1 && ARGS[1] == "remove"
   #TODO remove entry from "static_IPs.cfg" and free the static IP address
   println("Not implemented yet.")
