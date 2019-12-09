13c5d5d835d529df94775582542a2dabf01edbdd
Akito <akito.kitsune@protonmail.com>
2019-12-09 14:23:31 +0100

    Added Julia installation to init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # See LICENSE.
 # Copyright (C) 2019 Akito <akito.kitsune@protonmail.com>
+cd /root && \
+curl -sSL https://julialang-s3.julialang.org/bin/freebsd/x64/1.3/julia-1.3.0-freebsd-x86_64.tar.gz \
+  -o julia-1.3.0-freebsd-x86_64.tar.gz && \
+tar zxf julia-1.3.0-freebsd-x86_64.tar.gz && \
+rm julia-1.3.0-freebsd-x86_64.tar.gz && \
+ln -s /root/julia-1.3.0/bin/julia /usr/bin/julia && \
 mkdir /var/etc/openvpn/ccd && \
 #touch /var/etc/openvpn/ccd/clients.cfg && \
 curl -sSL https://raw.githubusercontent.com/Akito13/julia-serving-hookers/master/ovpn_adduser.sh \
